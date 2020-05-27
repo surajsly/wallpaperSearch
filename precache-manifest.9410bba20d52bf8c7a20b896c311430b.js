5a231db1fb6ea4049691eeb4d9b069063b0dd39f
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-05-27 08:36:49 +0530

    Updates

---
+++ precache-manifest.9410bba20d52bf8c7a20b896c311430b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ab1e18b1901bdc9222ac85f31c1479",
+    "url": "/wallpaperSearch/index.html"
+  },
+  {
+    "revision": "59540412913c95cce933",
+    "url": "/wallpaperSearch/static/css/main.5dd1bc62.chunk.css"
+  },
+  {
+    "revision": "576239bdd47165c3f765",
+    "url": "/wallpaperSearch/static/js/2.f88abdb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wallpaperSearch/static/js/2.f88abdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59540412913c95cce933",
+    "url": "/wallpaperSearch/static/js/main.e770dc62.chunk.js"
+  },
+  {
+    "revision": "97fecc34e67e2801d74d",
+    "url": "/wallpaperSearch/static/js/runtime-main.e749107d.js"
+  }
+]);
